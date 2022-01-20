10ac8cf24c60eff8ae3c942c342f646feedee83c
JiasenZheng <jiasenzheng2020@u.northwestern.edu>
2022-01-19 19:26:22 -0600

    complete tf broadcaster

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -34,9 +34,11 @@ static ros::Publisher js_pub;
 static long count;
 static ros::ServiceServer reset_service;
 static sensor_msgs::JointState js;
-static tf2_ros::TransformBroadcaster br;
-// static std::string js_left;
-// static std::string js_right;
+static double x_0;
+static double y_0;
+static double theta_0;
+
+
 
 
 
@@ -51,29 +53,49 @@ bool reset_callback(std_srvs::Trigger::Request &req,
     return true;
 }
 
-// void poseCallback()
-// {
-//     static tf2_ros::TransformBroadcaster br;
-    
-// }
 
 
 int main(int argc, char * argv[])
 {
     ros::init(argc, argv, "nusim");
     ros::NodeHandle nh;
+    static tf2_ros::TransformBroadcaster br;
     time_pub = nh.advertise<std_msgs::UInt64>("timestep",100);
     js_pub = nh.advertise<sensor_msgs::JointState>("red/joint_states",100);
     reset_service = nh.advertiseService("reset",reset_callback);
     ros::Rate loop_rate(rate);
     count = 0;
-    // js.header.stamp = ros::Time::now();
     js.name.push_back("red-wheel_left_joint");
     js.name.push_back("red-wheel_right_joint");
     js.position.push_back(0);
     js.position.push_back(0);
+
+    nh.param("x0",x_0,0.0);
+    nh.param("y0",y_0,0.0);
+    nh.param("theta0",theta_0,0.0);
+
+
     while (ros::ok())
     {
+        // construct a transform
+        geometry_msgs::TransformStamped trans;
+        
+        trans.header.stamp = ros::Time::now();
+        trans.child_frame_id = "red-base_footprint";
+        trans.header.frame_id = "world";
+
+        trans.transform.translation.x = x_0;
+        trans.transform.translation.y = y_0;
+        trans.transform.translation.z = 0;
+
+        tf2::Quaternion q;
+        q.setRPY(0,0,theta_0);
+        trans.transform.rotation.x = q.x();
+        trans.transform.rotation.y = q.y();
+        trans.transform.rotation.z = q.z();
+        trans.transform.rotation.w = q.w();
+        br.sendTransform(trans); 
+
         count++;
         js.header.stamp = ros::Time::now();
         timestep.data = count/rate;
