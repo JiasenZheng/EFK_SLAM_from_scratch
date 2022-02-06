2eadf6a4463758e5525b4868824518887dd97e1d
JiasenZheng <jiasenzheng2020@u.northwestern.edu>
2022-02-06 03:17:05 -0600

    Update cmakelist

--- nuturtle_control/src/circle.cpp
+++ nuturtle_control/src/circle.cpp
@@ -16,4 +16,20 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 
+static int frequency = 100;
 static ros::Publisher cmd_pub;
+static ros::ServiceServer control;
+static ros::ServiceServer reverse;
+static ros::ServiceServer stop;
+static geometry_msgs::Twist twist;
+
+int main(int argc, char** argv)
+{
+    ros::init(argc,argv,"circle");
+    ros::NodeHandle nh;
+
+    // Initialize publisher and services
+    cmd_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",100);
+
+
+}
