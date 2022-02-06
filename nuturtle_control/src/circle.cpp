ba618657efbc824c07026f667ce40ccd42a5cdee
JiasenZheng <jiasenzheng2020@u.northwestern.edu>
2022-02-06 04:12:17 -0600

    add control service

--- nuturtle_control/src/circle.cpp
+++ nuturtle_control/src/circle.cpp
@@ -23,6 +23,8 @@ static ros::ServiceServer reverse;
 static ros::ServiceServer stop;
 static geometry_msgs::Twist twist;
 
+
+
 int main(int argc, char** argv)
 {
     ros::init(argc,argv,"circle");
@@ -30,6 +32,15 @@ int main(int argc, char** argv)
 
     // Initialize publisher and services
     cmd_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",100);
+    control = nh.advertiseService("/control",control_callback);
+    reverse = nh.advertiseService("/reserve",reverse_callback);
+    stop = nh.advertiseService("/stop",stop_callback);
 
-
+    ros::Rate loop_rate(frequency);
+    while(ros::ok())
+    {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
 }
--- nuturtle_control/src/odometry.cpp
+++ nuturtle_control/src/odometry.cpp
@@ -144,5 +144,5 @@ int main(int argc, char** argv)
         ros::spinOnce();
         loop_rate.sleep();
     }
-
+    return 0;
 }
